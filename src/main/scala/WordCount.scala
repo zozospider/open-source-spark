3afb8fab83189b6e527cbda0f44fc4d7e2e9fe27
zozospider <2642220896@qq.com>
2021-05-10 23:11:44 +0800

    update open-source-spark

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -7,11 +7,11 @@ object WordCount {
     val conf: SparkConf = new SparkConf().setAppName("WordCount").setMaster("local[*]")
     val context: SparkContext = new SparkContext(conf)
 
-    val rdd: RDD[String] = context.textFile("data-dir\\word-count")
+    val rdd: RDD[String] = context.textFile(path = "data-dir\\word-count")
     val rdd2: RDD[String] = rdd.flatMap((s: String) => s.split(" "))
     val rdd3: RDD[(String, Int)] = rdd2.map((s: String) => (s, 1))
     val rdd4: RDD[(String, Int)] = rdd3.reduceByKey((i1: Int, i2: Int) => i1 + i2)
-    rdd4.collect.foreach(println)
+    rdd4.saveAsTextFile(path = "output")
 
     context.stop
   }
